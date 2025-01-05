ba75bf81d185498731036d1724e719133e35f42d
Ifwtzzzzzh <nosarahmanda.rplb@gmail.com>
2025-01-05 11:47:00 +0700

    feat: add multi auth using breeze

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])
+    ->name('admin.dashboard')
+    ->middleware(['auth', 'role:admin']);
+Route::get('/user/dashboard', [UserController::class, 'dashboard'])
+    ->name('user.dashboard')
+    ->middleware(['auth', 'role:user']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
