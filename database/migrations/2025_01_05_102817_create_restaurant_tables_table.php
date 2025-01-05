610c4dd07ee76c9f0ccfd7822c7ea27a90b9c2d9
Ifwtzzzzzh <nosarahmanda.rplb@gmail.com>
2025-01-05 17:36:02 +0700

    feat: add database migrations

---
+++ database/migrations/2025_01_05_102817_create_restaurant_tables_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('restaurant_tables', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('restaurant_id')->constrained()->cascadeOnDelete();
+            $table->tinyInteger('capacity');
+            $table->boolean('is_available');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restaurant_tables');
+    }
+};
